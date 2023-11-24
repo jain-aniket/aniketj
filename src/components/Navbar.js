3cf071bcc7ebf78115a728f155b1ab7a42d10163
jain-aniket <me.aniket.jain@gmail.com>
2023-11-24 15:37:26 -0500

    Light/Dark Toggle

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
+import sun from '../assets/sun.svg';
+import moon from '../assets/moon.svg';
 
 const Navbar = () => {
+  const [isDay, setIsDay] = useState(true);
   return (
     <nav>
       <a href="/">Aniket Jain</a>
@@ -9,6 +12,7 @@ const Navbar = () => {
         <a href="#">Projects</a>
         <a href="#">Astro + Aniket</a>
         <a href="#">About</a>
+        <img src={isDay ? sun : moon} alt='Toggle day/night' onClick={() => setIsDay(!isDay)} />
       </div>
     </nav>
   );
